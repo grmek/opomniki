e45e2145164f68c3d17735483271e7af329cce44
Rok Grmek <grmekrok@gmail.com>
2016-03-08 09:41:21 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -16,7 +16,7 @@ window.addEventListener('load', function() {
 		document.querySelector("#naziv_opomnika").value = "";
 		document.querySelector("#cas_opomnika").value = "";
 		document.querySelector("#opomniki").innerHTML += "\
-			<div class='opomnik'>\
+			<div class='opomnik senca rob'>\
             	<div class='naziv_opomnika'>" + naziv + "</div>\
             	<div class='cas_opomnika'> Opomnik čez <span>" + cas + "</span> sekund.</div>\
 			</div>";
@@ -27,14 +27,23 @@ window.addEventListener('load', function() {
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
-	
+			
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if(cas == 0){
+				var naziv = document.querySelector(".naziv_opomnika").innerHTML;
+				alert("Opomnik!\n\nZadolžitev " + naziv + " je potekla!")
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
+			else{
+				casovnik.innerHTML = cas - 1;
+			}
+			
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
